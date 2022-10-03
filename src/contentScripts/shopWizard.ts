dc0eec19b1c2c0270c4f7afa43e49ef4f6248b44
Dave Lu <davelu@yelp.com>
2022-10-03 01:53:47 -0700

    Temp: savepoint

--- src/contentScripts/shopWizard.ts
+++ src/contentScripts/shopWizard.ts
@@ -1,170 +1,32 @@
-import { normalDelay, randomPercentRange, sleep } from "@src/util/randomDelay";
+import browser from "webextension-polyfill";
+import { randomPercentRange, sleep } from "@src/util/randomDelay";
 import { assume } from "@src/util/typeAssertions";
 import {
     getItemsToPrice,
     popNextItemToPrice,
-    pushItemsToPrice,
     pushNextItemToPrice,
 } from "@src/pricingQueue";
-import {
-    getListings,
-    ListingData,
-    upsertListingsSection,
-} from "@src/database/listings";
+import { ListingData } from "@src/database/listings";
 import { getSetting } from "@src/util/localStorage";
-import { switchToUnbannedAccount } from "@src/accounts";
-import { getNextItemsToReprice } from "@src/priceMonitoring";
-import { queueUserShopToVisit } from "@src/userShopQueue";
-import { waitReady } from "@src/util/domHelpers";
-import { migrateContentScriptDBToBackground } from "@src/background/migration";
-import { ljs } from "@src/util/logging";
+import { $, $All } from "@src/util/domHelpers";
+import { getSection } from "@src/shopWizardSection";
 
-function priceFromRow(row: HTMLElement): ListingData {
-    const [userName, _, quantity, __, price] = row.innerText.split("\n");
+browser.runtime.onMessage.addListener(
+    (request: { action: "SEARCH_SHOP_WIZARD" } & SearchShopWizard) => {
+        if (request.action === "SEARCH_SHOP_WIZARD") {
+            return searchShopWizard(request);
+        }
+    },
+);
 
-    return {
-        userName,
-        quantity: parseInt(quantity),
-        link: assume(row.querySelector("a")).href,
-        price: parseInt(price.replaceAll(",", "")),
-    };
+function askForAnotherSection() {
+    const resubmitButton = assume(
+        document.querySelector<HTMLElement>("#resubmitWizard"),
+    );
+    resubmitButton.click();
 }
 
-const REFRESH_DELAY = 537;
-
-async function logListings(itemName: string) {
-    ljs(await getListings(itemName, 5));
-}
-
-function hoursAgo(epochMillis: number) {
-    return (Date.now() - epochMillis) / (1000 * 60 * 60);
-}
-
-// If user X is the cheapest, and then their section is cleared out,
-// it now becomes impossible to find a listing page that would clobber
-// out their original result. We can manually visit their shop to clear
-// it instead
-async function clearAnyInvalidListingsInFront(itemName: string) {
-    const listings = await getListings(itemName, 6);
-    for (const listing of listings) {
-        // The next price is fresh, we can exit
-        if (hoursAgo(listing.lastSeen) < 1) {
-            return;
-        }
-        // It's old, we failed to find it's section. Check that it's
-        // still valid, to unclog potential invalid listings
-        queueUserShopToVisit(listing.link);
-    }
-}
-
-function checkPrice(
-    itemName = "One Dubloon Coin",
-    numberOfUniquePagesToCheck = 6,
-    maxNumberOfRequests = 10,
-) {
-    console.log("Checking price for ", itemName);
-    let requestNumber = 0;
-    let uniquePages = 0;
-    const prices: { [userName: string]: ListingData } = {};
-
-    async function refreshPrices() {
-        requestNumber += 1;
-        console.log("Checking prices", requestNumber, uniquePages);
-        await sleep(randomPercentRange(REFRESH_DELAY, 0.8));
-        const resubmitButton = assume(
-            document.querySelector<HTMLElement>("#resubmitWizard"),
-        );
-        resubmitButton.click();
-    }
-
-    async function savePrices() {
-        const numberOfPrices = Object.keys(prices).length;
-
-        const listings: ListingData[] = [];
-        document
-            .querySelectorAll<HTMLElement>(
-                "#shopWizardFormResults li:not(.wizard-results-grid-header)",
-            )
-            .forEach((row) => {
-                const price = priceFromRow(row);
-                prices[price.userName] = price;
-
-                listings.push(price);
-            });
-
-        await upsertListingsSection(itemName, listings);
-
-        const newPricesWereFound = Object.keys(prices).length > numberOfPrices;
-        if (newPricesWereFound) {
-            uniquePages += 1;
-        }
-    }
-
-    const observer = new MutationObserver(async () => {
-        if (
-            assume(
-                document.querySelector<HTMLElement>("#shopWizardFormResults"),
-            ).innerText.includes("too many searches")
-        ) {
-            // Re-push, so we can retry after the ban is lifted
-            pushNextItemToPrice(itemName);
-            await switchToUnbannedAccount();
-        }
-
-        await savePrices();
-
-        const sortedPrices = Object.values(prices).sort(
-            (priceA, priceB) => priceA.price - priceB.price,
-        );
-
-        const itemIsWorthless =
-            sortedPrices[0] && sortedPrices[0].price < priceThreshold.get();
-        if (itemIsWorthless) {
-            console.log(
-                `${itemName} is worthless with a price of ${sortedPrices[0].price}`,
-            );
-        }
-
-        if (
-            requestNumber >= maxNumberOfRequests ||
-            uniquePages >= numberOfUniquePagesToCheck ||
-            itemIsWorthless
-        ) {
-            if (uniquePages === 0) {
-                // The item is possibly unbuyable
-                await upsertListingsSection(itemName, [
-                    {
-                        userName: "NOT_A_REAL_USER!",
-                        link: "NOT_A_REAL_LINK!",
-                        quantity: 1,
-                        price: 1000000,
-                    },
-                ]);
-            }
-
-            // The item is possibly unbuyable
-            await clearAnyInvalidListingsInFront(itemName);
-
-            observer.disconnect();
-
-            logListings(itemName);
-
-            await sleep(randomPercentRange(delay.get(), 0.8));
-            document
-                .querySelectorAll<HTMLElement>(".wizard-button__2020")[1]
-                .click();
-            return;
-        }
-
-        await refreshPrices();
-    });
-
-    observer.observe(assume(document.querySelector("#shopWizardFormResults")), {
-        attributes: true,
-        childList: true,
-        subtree: true,
-    });
-
+function searchItem(itemName: string) {
     const itemInput = assume(
         document.querySelector<HTMLInputElement>("#shopwizard"),
     );
@@ -172,26 +34,112 @@ function checkPrice(
     assume(document.querySelector<HTMLElement>("#submit_wizard")).click();
 }
 
-async function processQueueItem() {
-    await waitReady();
-    await normalDelay(1234);
+function scrapeSection(): ListingData[] {
+    return $All(
+        "#shopWizardFormResults li:not(.wizard-results-grid-header)",
+    ).map((row) => {
+        const [userName, _, quantity, __, price] = row.innerText.split("\n");
 
-    const itemName = popNextItemToPrice();
-    if (!itemName) {
-        console.log(
-            "No items in the pricing queue, calculating the best items to re-price...",
+        return {
+            userName,
+            quantity: parseInt(quantity),
+            link: assume(row.querySelector("a")).href,
+            price: parseInt(price.replaceAll(",", "")),
+        };
+    });
+}
+
+export type SearchShopWizard = {
+    itemName: string;
+    numberOfSections?: number;
+    maxRequests?: number;
+    abortIfCheaperThan?: number;
+};
+
+export type ShopWizardResult = {
+    sections: ShopWizardSection[];
+    tooManySearches?: true;
+};
+
+export type ShopWizardSection = ListingData[];
+
+async function searchShopWizard({
+    itemName,
+    numberOfSections = 6,
+    maxRequests = 10,
+    abortIfCheaperThan = priceThreshold.get(),
+}: SearchShopWizard): Promise<ShopWizardResult> {
+    console.log("Checking price for ", itemName);
+    let requestNumber = 0;
+    let cheapestPrice = 1_000_000;
+    const sections: { [section: number]: ListingData[] } = {};
+
+    return new Promise((resolve) => {
+        const observer = new MutationObserver(async () => {
+            requestNumber += 1;
+
+            if (
+                assume($("#shopWizardFormResults")).innerText.includes(
+                    "too many searches",
+                )
+            ) {
+                observer.disconnect();
+                resolve({
+                    sections: Object.values(sections),
+                    tooManySearches: true,
+                });
+                return;
+            }
+
+            const listings = scrapeSection();
+            if (listings.length === 0) {
+                return;
+            }
+
+            sections[getSection(listings[0].userName)] = listings;
+            cheapestPrice = Math.min(cheapestPrice, listings[0].price);
+
+            const sectionsFound = Object.keys(sections).length;
+            console.log("Checked ", requestNumber, sectionsFound);
+            if (
+                requestNumber >= maxRequests ||
+                sectionsFound >= numberOfSections ||
+                cheapestPrice < abortIfCheaperThan
+            ) {
+                if (sectionsFound === 0) {
+                    // The item is possibly unbuyable
+                    sections[0] = [
+                        {
+                            userName: "NOT_A_REAL_USER!",
+                            link: "NOT_A_REAL_LINK!",
+                            quantity: 1,
+                            price: 1000000,
+                        },
+                    ];
+                }
+
+                observer.disconnect();
+                resolve({
+                    sections: Object.values(sections),
+                });
+                return;
+            }
+
+            await sleep(randomPercentRange(555, 0.8));
+            askForAnotherSection();
+        });
+
+        observer.observe(
+            assume(document.querySelector("#shopWizardFormResults")),
+            {
+                attributes: true,
+                childList: true,
+                subtree: true,
+            },
         );
-        const itemsToReprice = await getNextItemsToReprice(100, 50);
-        pushItemsToPrice(itemsToReprice);
-        if (itemsToReprice.length === 0) {
-            // Wait an hour
-            await normalDelay(1000 * 60 * 60);
-        }
-        processQueueItem();
-        return;
-    }
 
-    checkPrice(itemName);
+        searchItem(itemName);
+    });
 }
 
 const isAutoProcessingQueue = getSetting(
@@ -202,21 +150,7 @@ const isAutoProcessingQueue = getSetting(
 
 const priceThreshold = getSetting("abortIfUnderPrice", 1000, parseInt);
 
-const delay = getSetting("shopWizardSearchDelay", 1000, parseInt);
-
-function submitMissingOrStaleItemsToPrice(itemNames: string[]) {
-    pushItemsToPrice([]);
-    return Promise.all(
-        itemNames.map(async (item) => {
-            const listings = await getListings(item);
-            if (listings.length === 0) {
-                console.log("Submit ", item);
-                pushNextItemToPrice(item);
-                return;
-            }
-        }),
-    );
-}
+const delayBetweenRetry = getSetting("shopWizardSearchDelay", 1000, parseInt);
 
 function refreshHUD() {
     const overlay = document.createElement("div");
@@ -271,7 +205,6 @@ function refreshHUD() {
             // wait for pasted content to show up
             setTimeout(async () => {
                 const itemName = enqueueItemInput.value.trim();
-                logListings(itemName);
                 pushNextItemToPrice(itemName);
                 overlay.remove();
                 refreshHUD();
@@ -283,8 +216,9 @@ function refreshHUD() {
     const delayInput = document.createElement("input");
     delayInput.type = "text";
     delayInput.placeholder = "Delay";
-    delayInput.value = delay.get().toString();
-    delayInput.onchange = () => delay.set(parseInt(delayInput.value));
+    delayInput.value = delayBetweenRetry.get().toString();
+    delayInput.onchange = () =>
+        delayBetweenRetry.set(parseInt(delayInput.value));
     overlay.appendChild(delayInput);
 
     const skipButton = document.createElement("button");
@@ -304,21 +238,7 @@ function refreshHUD() {
     thresholdInput.onchange = () =>
         priceThreshold.set(parseInt(thresholdInput.value));
     overlay.appendChild(thresholdInput);
-
-    const newItemsTextArea = document.createElement("textarea");
-    newItemsTextArea.onchange = () => {
-        const itemsToStartMonitoring = newItemsTextArea.value
-            .trim()
-            .split("\n");
-        submitMissingOrStaleItemsToPrice(itemsToStartMonitoring);
-    };
-    overlay.appendChild(newItemsTextArea);
 }
 
-refreshHUD();
-
-if (isAutoProcessingQueue.get()) {
-    processQueueItem();
-} else {
-    migrateContentScriptDBToBackground();
-}
+// refreshHUD();
+// migrateContentScriptDBToBackground();
